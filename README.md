356c68bf811e042cdc9f44352dcb7473cc4ab7f0
Keiichiro Miyamoto <miyamotok0105@gmail.com>
2017-09-20 05:55:16 +0900

    add

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# 参考
+
+
+地図検索, カメラ投稿, タイマー, 日出時刻検索, ARアプリ, AIアプリなど実用アプリを20個以上作って学びます。CoreNFCなどiOS 11新機能も順次収録・追加中。Xcode 9, Swift 4を初心者が基礎から学べます。
+
+https://www.udemy.com/ios11basics/learn/v4/overview
---
+++ coreMl1/coreMl1/ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  coreMl1
+//
+//  Created by USER on 2017/09/17.
+//  Copyright © 2017年 USER. All rights reserved.
+//
+
+import UIKit
+import Vision
+import CoreML
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var photoDisplay: UIImageView!
+    @IBOutlet weak var photoInfoDisplay: UITextView!
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func takePhoto(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        photoDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageInference(image: (info[UIImagePickerControllerOriginalImage] as? UIImage)!)
+    }
+    
+    func imageInference(image: UIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Resnet50().model)  else {
+            fatalError("modelをロードできません。")
+        }
+        
+        // リクエスト（VNCoreMLRequest）の生成とハンドラ処理
+        let request = VNCoreMLRequest(model: model) { request, error in
+            
+            guard let results = request.results as? [VNClassificationObservation],
+                let firstResult = results.first else {
+                fatalError("Error results")
+            }
+
+
+            DispatchQueue.main.async {
+                self.photoInfoDisplay.text = "確率 = \(Int(firstResult.confidence * 100))%(, \n 詳細 \((firstResult.identifier))"
+            }
+            
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("Error convert CIImage")
+            }
+            
+            let imageHandler = VNImageRequestHandler(ciImage: ciImage)
+            
+            DispatchQueue.global(qos: .userInteractive).async {
+                do {
+                    try imageHandler.perform([request])
+                } catch {
+                    print("error")
+                }
+                
+            }
+            
+            
+//            if let classification = results.first {
+//                print("identifier = \(classification.identifier)")
+//                print("confidence = \(classification.confidence)")
+//            } else {
+//                print("error")
+//            }
+        }
+        
+        // CIImageへの変換
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Error convert CIImage")
+        }
+        
+        // ハンドラの生成と実行
+        let handler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        guard (try? handler.perform([request])) != nil else {
+            fatalError("Error handler.perform")
+        }
+        
+        
+//        let request = VNCoreMLRequest(model) {
+//            [weak self] request, error in
+//
+//            guard let requests = request.results as? [VNClassificationObservation],
+//                let firstResult = result.fir
+//        }
+        
+        
+        
+//        guard let model = try? VNCoreMLModel(
+    }
+}
+
